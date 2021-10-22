209ed7f537efcc3ee1bcfa50bc6a00fc78e65904
Lucas <lucasmellocem12@gmail.com>
2021-10-22 09:12:48 -0300

    Now server can be started without problems.

--- src/main/java/com/github/lehjr/powersuits/basemod/ClientObjects.java
+++ src/main/java/com/github/lehjr/powersuits/basemod/ClientObjects.java
@@ -1,4 +1,42 @@
 package com.github.lehjr.powersuits.basemod;
 
+import com.github.lehjr.powersuits.client.control.KeybindKeyHandler;
+import com.github.lehjr.powersuits.client.event.ClientTickHandler;
+import com.github.lehjr.powersuits.client.event.ModelBakeEventHandler;
+import com.github.lehjr.powersuits.client.event.RenderEventHandler;
+import com.github.lehjr.powersuits.client.gui.modding.module.craft_install_salvage.CraftInstallSalvageGui;
+import com.github.lehjr.powersuits.client.render.entity.LuxCapacitorEntityRenderer;
+import com.github.lehjr.powersuits.client.render.entity.PlasmaBoltEntityRenderer;
+import com.github.lehjr.powersuits.client.render.entity.RailGunBoltRenderer;
+import com.github.lehjr.powersuits.client.render.entity.SpinningBladeEntityRenderer;
+import com.github.lehjr.powersuits.event.LogoutEventHandler;
+import com.github.lehjr.powersuits.event.PlayerLoginHandler;
+import net.minecraft.client.gui.ScreenManager;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.client.registry.RenderingRegistry;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
 public class ClientObjects {
+	public static void register() {
+		IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+		modEventBus.addListener(ModelBakeEventHandler.INSTANCE::onModelBake);
+		modEventBus.addListener(RenderEventHandler.INSTANCE::preTextureStitch);
+
+		MinecraftForge.EVENT_BUS.register(RenderEventHandler.INSTANCE);
+		MinecraftForge.EVENT_BUS.register(new ClientTickHandler());
+		MinecraftForge.EVENT_BUS.register(new KeybindKeyHandler());
+		MinecraftForge.EVENT_BUS.register(new LogoutEventHandler());
+
+		MinecraftForge.EVENT_BUS.addListener(PlayerLoginHandler::onPlayerLoginClient);// just to populated keybinds -_-
+
+		RenderingRegistry.registerEntityRenderingHandler(MPSObjects.RAILGUN_BOLT_ENTITY_TYPE.get(), RailGunBoltRenderer::new);
+		RenderingRegistry.registerEntityRenderingHandler(MPSObjects.LUX_CAPACITOR_ENTITY_TYPE.get(), LuxCapacitorEntityRenderer::new);
+		RenderingRegistry.registerEntityRenderingHandler(MPSObjects.PLASMA_BALL_ENTITY_TYPE.get(), PlasmaBoltEntityRenderer::new);
+		RenderingRegistry.registerEntityRenderingHandler(MPSObjects.SPINNING_BLADE_ENTITY_TYPE.get(), SpinningBladeEntityRenderer::new);
+
+		//        ScreenManager.registerFactory(MPSObjects.MODULE_CONFIG_CONTAINER_TYPE, TinkerModuleGui::new);
+		ScreenManager.register(MPSObjects.SALVAGE_CRAFT_CONTAINER_TYPE.get(), CraftInstallSalvageGui::new);
+	}
 }
--- src/main/java/com/github/lehjr/powersuits/basemod/ModularPowersuits.java
+++ src/main/java/com/github/lehjr/powersuits/basemod/ModularPowersuits.java
@@ -35,21 +35,14 @@ import com.github.lehjr.numina.util.capabilities.module.rightclick.IRightClickMo
 import com.github.lehjr.numina.util.capabilities.module.rightclick.RightClickModule;
 import com.github.lehjr.numina.util.capabilities.module.toggleable.IToggleableModule;
 import com.github.lehjr.numina.util.capabilities.module.toggleable.ToggleableModule;
-import com.github.lehjr.powersuits.client.control.KeybindKeyHandler;
-import com.github.lehjr.powersuits.client.event.ClientTickHandler;
-import com.github.lehjr.powersuits.client.event.ModelBakeEventHandler;
-import com.github.lehjr.powersuits.client.event.RenderEventHandler;
-import com.github.lehjr.powersuits.client.gui.modding.module.craft_install_salvage.CraftInstallSalvageGui;
-import com.github.lehjr.powersuits.client.render.entity.LuxCapacitorEntityRenderer;
-import com.github.lehjr.powersuits.client.render.entity.PlasmaBoltEntityRenderer;
-import com.github.lehjr.powersuits.client.render.entity.RailGunBoltRenderer;
-import com.github.lehjr.powersuits.client.render.entity.SpinningBladeEntityRenderer;
 import com.github.lehjr.powersuits.config.MPSSettings;
 import com.github.lehjr.powersuits.constants.MPSConstants;
 import com.github.lehjr.powersuits.constants.MPSRegistryNames;
-import com.github.lehjr.powersuits.event.*;
+import com.github.lehjr.powersuits.event.EntityDamageEvent;
+import com.github.lehjr.powersuits.event.HarvestEventHandler;
+import com.github.lehjr.powersuits.event.MovementManager;
+import com.github.lehjr.powersuits.event.PlayerUpdateHandler;
 import com.github.lehjr.powersuits.network.MPSPackets;
-import net.minecraft.client.gui.ScreenManager;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.inventory.container.SimpleNamedContainerProvider;
@@ -70,7 +63,6 @@ import net.minecraftforge.event.AttachCapabilitiesEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.ModLoadingContext;
-import net.minecraftforge.fml.client.registry.RenderingRegistry;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
@@ -83,157 +75,139 @@ import javax.annotation.Nullable;
 
 @Mod(MPSConstants.MOD_ID)
 public class ModularPowersuits {
-    public ModularPowersuits() {
-        // Config
-        ModLoadingContext.get().registerConfig(ModConfig.Type.CLIENT, MPSSettings.CLIENT_SPEC, ConfigHelper.setupConfigFile("mps-client-only.toml", MPSConstants.MOD_ID).getAbsolutePath());
-        ModLoadingContext.get().registerConfig(ModConfig.Type.SERVER, MPSSettings.SERVER_SPEC); // note config file location for dedicated server is stored in the world config
+	public ModularPowersuits() {
+		// Config
+		ModLoadingContext.get().registerConfig(ModConfig.Type.CLIENT, MPSSettings.CLIENT_SPEC, ConfigHelper.setupConfigFile("mps-client-only.toml", MPSConstants.MOD_ID).getAbsolutePath());
+		ModLoadingContext.get().registerConfig(ModConfig.Type.SERVER, MPSSettings.SERVER_SPEC); // note config file location for dedicated server is stored in the world config
 
-        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+		IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
-        // Register the setup method for modloading
-        modEventBus.addListener(this::setup);
+		// Register the setup method for modloading
+		modEventBus.addListener(this::setup);
 
-        // Register the doClientStuff method for modloading
-        modEventBus.addListener(this::setupClient);
+		// Register the doClientStuff method for modloading
+		modEventBus.addListener(this::setupClient);
 
-        // Register ourselves for server and other game events we are interested in
-        MinecraftForge.EVENT_BUS.register(this);
-        MinecraftForge.EVENT_BUS.addListener(EntityDamageEvent::handleEntityDamageEvent);
-        MinecraftForge.EVENT_BUS.addListener(EntityDamageEvent::entityAttackEventHandler);
-        MinecraftForge.EVENT_BUS.register(new PlayerUpdateHandler());
-        MinecraftForge.EVENT_BUS.register(MovementManager.INSTANCE);
+		// Register ourselves for server and other game events we are interested in
+		MinecraftForge.EVENT_BUS.register(this);
+		MinecraftForge.EVENT_BUS.addListener(EntityDamageEvent::handleEntityDamageEvent);
+		MinecraftForge.EVENT_BUS.addListener(EntityDamageEvent::entityAttackEventHandler);
+		MinecraftForge.EVENT_BUS.register(new PlayerUpdateHandler());
+		MinecraftForge.EVENT_BUS.register(MovementManager.INSTANCE);
 
-        MinecraftForge.EVENT_BUS.addListener(HarvestEventHandler::handleHarvestCheck);
-        MinecraftForge.EVENT_BUS.addListener(HarvestEventHandler::handleBreakSpeed);
+		MinecraftForge.EVENT_BUS.addListener(HarvestEventHandler::handleHarvestCheck);
+		MinecraftForge.EVENT_BUS.addListener(HarvestEventHandler::handleBreakSpeed);
 
-        MPSObjects.ITEMS.register(modEventBus);
-        MPSObjects.BLOCKS.register(modEventBus);
-        MPSObjects.TILE_TYPES.register(modEventBus);
-        MPSObjects.ENTITY_TYPES.register(modEventBus);
-        MPSObjects.CONTAINER_TYPES.register(modEventBus);
+		MPSObjects.ITEMS.register(modEventBus);
+		MPSObjects.BLOCKS.register(modEventBus);
+		MPSObjects.TILE_TYPES.register(modEventBus);
+		MPSObjects.ENTITY_TYPES.register(modEventBus);
+		MPSObjects.CONTAINER_TYPES.register(modEventBus);
 
-        // handles loading and reloading event
-        modEventBus.addListener((ModConfig.ModConfigEvent event) -> {
-            new RuntimeException("Got config " + event.getConfig() + " name " + event.getConfig().getModId() + ":" + event.getConfig().getFileName());
+		// handles loading and reloading event
+		modEventBus.addListener((ModConfig.ModConfigEvent event) -> {
+			new RuntimeException("Got config " + event.getConfig() + " name " + event.getConfig().getModId() + ":" + event.getConfig().getFileName());
 
-            final ModConfig config = event.getConfig();
-            if (config.getSpec() == MPSSettings.SERVER_SPEC) {
-                MPSSettings.getModuleConfig().setServerConfig(config);
+			final ModConfig config = event.getConfig();
+			if (config.getSpec() == MPSSettings.SERVER_SPEC) {
+				MPSSettings.getModuleConfig().setServerConfig(config);
 
-                // This is actually for a feature that isn't even currently enabled :P
-                // getFullPath can't be used on client if it isn't also hosting the server
-                if (config instanceof CommentedFileConfig) {
-                    CosmeticPresetSaveLoad.setConfigDirString(config.getFullPath().getParent().toString());
-                    CosmeticPresetSaveLoad.copyPresetsFromJar(config.getFullPath().getParent().toString());
-                }
-            }
-        });
-    }
+				// This is actually for a feature that isn't even currently enabled :P
+				// getFullPath can't be used on client if it isn't also hosting the server
+				if (config instanceof CommentedFileConfig) {
+					CosmeticPresetSaveLoad.setConfigDirString(config.getFullPath().getParent().toString());
+					CosmeticPresetSaveLoad.copyPresetsFromJar(config.getFullPath().getParent().toString());
+				}
+			}
+		});
+	}
 
-    /**
-     * Setup common (clien/server) stuff
-     */
-    private void setup(final FMLCommonSetupEvent event) {
-        MPSPackets.registerMPAPackets();
-    }
+	/**
+	 * Setup common (clien/server) stuff
+	 */
+	private void setup(final FMLCommonSetupEvent event) {
+		MPSPackets.registerMPAPackets();
+	}
 
-    /**
-     * Setup client related stuff
-     */
-    private void setupClient(final FMLClientSetupEvent event) {
-        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
-
-        modEventBus.addListener(ModelBakeEventHandler.INSTANCE::onModelBake);
-        modEventBus.addListener(RenderEventHandler.INSTANCE::preTextureStitch);
-
-        MinecraftForge.EVENT_BUS.register(RenderEventHandler.INSTANCE);
-        MinecraftForge.EVENT_BUS.register(new ClientTickHandler());
-        MinecraftForge.EVENT_BUS.register(new KeybindKeyHandler());
-        MinecraftForge.EVENT_BUS.register(new LogoutEventHandler());
-
-        MinecraftForge.EVENT_BUS.addListener(PlayerLoginHandler::onPlayerLoginClient);// just to populated keybinds -_-
-
-        RenderingRegistry.registerEntityRenderingHandler(MPSObjects.RAILGUN_BOLT_ENTITY_TYPE.get(), RailGunBoltRenderer::new);
-        RenderingRegistry.registerEntityRenderingHandler(MPSObjects.LUX_CAPACITOR_ENTITY_TYPE.get(), LuxCapacitorEntityRenderer::new);
-        RenderingRegistry.registerEntityRenderingHandler(MPSObjects.PLASMA_BALL_ENTITY_TYPE.get(), PlasmaBoltEntityRenderer::new);
-        RenderingRegistry.registerEntityRenderingHandler(MPSObjects.SPINNING_BLADE_ENTITY_TYPE.get(), SpinningBladeEntityRenderer::new);
-
-//        ScreenManager.registerFactory(MPSObjects.MODULE_CONFIG_CONTAINER_TYPE, TinkerModuleGui::new);
-        ScreenManager.register(MPSObjects.SALVAGE_CRAFT_CONTAINER_TYPE.get(), CraftInstallSalvageGui::new);
+	/**
+	 * Setup client related stuff
+	 */
+	private void setupClient(final FMLClientSetupEvent event) {
+		ClientObjects.register();
 
 
-//        ScreenManager.register(MPSObjects.TINKERTABLE_CONTAINER_TYPE.get(), TinkerTableGui::new); // FIXME: replace with module craft/install/salvage GUI + module tweak GUI
+		//        ScreenManager.register(MPSObjects.TINKERTABLE_CONTAINER_TYPE.get(), TinkerTableGui::new); // FIXME: replace with module craft/install/salvage GUI + module tweak GUI
 
-//        ScreenManager.register(NuminaObjects.NUMINA_CRAFTING_CONTAINER_TYPE.get(), TinkerCraftingGUI::new);
+		//        ScreenManager.register(NuminaObjects.NUMINA_CRAFTING_CONTAINER_TYPE.get(), TinkerCraftingGUI::new);
 
 
-//        ClientRegistry.bindTileEntityRenderer(MPSObjects.TINKER_TABLE_TILE_TYPE.get(), TinkerTableRenderer::new); //?
-    }
+		//        ClientRegistry.bindTileEntityRenderer(MPSObjects.TINKER_TABLE_TILE_TYPE.get(), TinkerTableRenderer::new); //?
+	}
 
-    /**
-     * Attach capabilities to a few existing items in order to use them as modules
-     */
-    @SubscribeEvent
-    public void attachCapability(AttachCapabilitiesEvent<ItemStack> event) {
-        // Clock
-        if (!event.getCapabilities().containsKey(MPSRegistryNames.CLOCK_MODULE_REG) && event.getObject().getItem().equals(Items.CLOCK)) {
-            final ItemStack stack = event.getObject();
+	/**
+	 * Attach capabilities to a few existing items in order to use them as modules
+	 */
+	@SubscribeEvent
+	public void attachCapability(AttachCapabilitiesEvent<ItemStack> event) {
+		// Clock
+		if (!event.getCapabilities().containsKey(MPSRegistryNames.CLOCK_MODULE_REG) && event.getObject().getItem().equals(Items.CLOCK)) {
+			final ItemStack stack = event.getObject();
 
-            IToggleableModule clock = new ToggleableModule(stack, EnumModuleCategory.SPECIAL, EnumModuleTarget.HEADONLY, MPSSettings::getModuleConfig, true);
-            event.addCapability(MPSRegistryNames.CLOCK_MODULE_REG, new ICapabilityProvider() {
-                @Nonnull
-                @Override
-                public <T> LazyOptional<T> getCapability(@Nonnull Capability<T> cap, @Nullable Direction side) {
-                    if (cap == PowerModuleCapability.POWER_MODULE) {
-                        return LazyOptional.of(()->(T)clock);
-                    }
-                    return LazyOptional.empty();
-                }
-            });
+			IToggleableModule clock = new ToggleableModule(stack, EnumModuleCategory.SPECIAL, EnumModuleTarget.HEADONLY, MPSSettings::getModuleConfig, true);
+			event.addCapability(MPSRegistryNames.CLOCK_MODULE_REG, new ICapabilityProvider() {
+				@Nonnull
+				@Override
+				public <T> LazyOptional<T> getCapability(@Nonnull Capability<T> cap, @Nullable Direction side) {
+					if (cap == PowerModuleCapability.POWER_MODULE) {
+						return LazyOptional.of(() -> (T) clock);
+					}
+					return LazyOptional.empty();
+				}
+			});
 
-            // Compass
-        } else if (!event.getCapabilities().containsKey(MPSRegistryNames.COMPASS_MODULE_REG) && event.getObject().getItem().equals(Items.COMPASS)) {
-            final ItemStack stack = event.getObject();
-            IToggleableModule compass = new ToggleableModule(stack, EnumModuleCategory.SPECIAL, EnumModuleTarget.HEADONLY, MPSSettings::getModuleConfig, true);
+			// Compass
+		} else if (!event.getCapabilities().containsKey(MPSRegistryNames.COMPASS_MODULE_REG) && event.getObject().getItem().equals(Items.COMPASS)) {
+			final ItemStack stack = event.getObject();
+			IToggleableModule compass = new ToggleableModule(stack, EnumModuleCategory.SPECIAL, EnumModuleTarget.HEADONLY, MPSSettings::getModuleConfig, true);
 
-            event.addCapability(MPSRegistryNames.COMPASS_MODULE_REG, new ICapabilityProvider() {
-                @Nonnull
-                @Override
-                public <T> LazyOptional<T> getCapability(@Nonnull Capability<T> cap, @Nullable Direction side) {
-                    if (cap == PowerModuleCapability.POWER_MODULE) {
-                        return LazyOptional.of(()->(T)compass);
-                    }
-                    return LazyOptional.empty();
-                }
-            });
+			event.addCapability(MPSRegistryNames.COMPASS_MODULE_REG, new ICapabilityProvider() {
+				@Nonnull
+				@Override
+				public <T> LazyOptional<T> getCapability(@Nonnull Capability<T> cap, @Nullable Direction side) {
+					if (cap == PowerModuleCapability.POWER_MODULE) {
+						return LazyOptional.of(() -> (T) compass);
+					}
+					return LazyOptional.empty();
+				}
+			});
 
-            // Crafting workbench
-        } else if (!event.getCapabilities().containsKey(MPSRegistryNames.PORTABLE_WORKBENCH_MODULE_REG) && event.getObject().getItem().equals(Items.CRAFTING_TABLE)) {
-            final ItemStack stack = event.getObject();
-            final ITextComponent CONTAINER_NAME = new TranslationTextComponent("container.crafting");
-            IRightClickModule rightClick = new RightClickModule(stack, EnumModuleCategory.TOOL, EnumModuleTarget.TOOLONLY, MPSSettings::getModuleConfig) {
-                @Override
-                public ActionResult onItemRightClick(ItemStack itemStackIn, World worldIn, PlayerEntity playerIn, Hand hand) {
-                    if (!worldIn.isClientSide()) {
-                        NetworkHooks.openGui((ServerPlayerEntity) playerIn,
-                                new SimpleNamedContainerProvider((id, inventory, player) ->
-                                        new WorkbenchContainer(id, inventory)/*, IWorldPosCallable.of(worldIn, playerIn.getPosition()))*/, MPSConstants.CRAFTING_TABLE_CONTAINER_NAME));
-                        return ActionResult.success(itemStackIn);
-                    }
-                    return ActionResult.consume(itemStackIn);
-                }
-            };
+			// Crafting workbench
+		} else if (!event.getCapabilities().containsKey(MPSRegistryNames.PORTABLE_WORKBENCH_MODULE_REG) && event.getObject().getItem().equals(Items.CRAFTING_TABLE)) {
+			final ItemStack stack = event.getObject();
+			final ITextComponent CONTAINER_NAME = new TranslationTextComponent("container.crafting");
+			IRightClickModule rightClick = new RightClickModule(stack, EnumModuleCategory.TOOL, EnumModuleTarget.TOOLONLY, MPSSettings::getModuleConfig) {
+				@Override
+				public ActionResult onItemRightClick(ItemStack itemStackIn, World worldIn, PlayerEntity playerIn, Hand hand) {
+					if (!worldIn.isClientSide()) {
+						NetworkHooks.openGui((ServerPlayerEntity) playerIn,
+								new SimpleNamedContainerProvider((id, inventory, player) ->
+										                                 new WorkbenchContainer(id, inventory)/*, IWorldPosCallable.of(worldIn, playerIn.getPosition()))*/, MPSConstants.CRAFTING_TABLE_CONTAINER_NAME));
+						return ActionResult.success(itemStackIn);
+					}
+					return ActionResult.consume(itemStackIn);
+				}
+			};
 
-            event.addCapability(MPSRegistryNames.PORTABLE_WORKBENCH_MODULE_REG, new ICapabilityProvider() {
-                @Nonnull
-                @Override
-                public <T> LazyOptional<T> getCapability(@Nonnull Capability<T> cap, @Nullable Direction side) {
-                    if (cap == PowerModuleCapability.POWER_MODULE) {
-                        return LazyOptional.of(()->(T)rightClick);
-                    }
-                    return LazyOptional.empty();
-                }
-            });
-        }
-    }
+			event.addCapability(MPSRegistryNames.PORTABLE_WORKBENCH_MODULE_REG, new ICapabilityProvider() {
+				@Nonnull
+				@Override
+				public <T> LazyOptional<T> getCapability(@Nonnull Capability<T> cap, @Nullable Direction side) {
+					if (cap == PowerModuleCapability.POWER_MODULE) {
+						return LazyOptional.of(() -> (T) rightClick);
+					}
+					return LazyOptional.empty();
+				}
+			});
+		}
+	}
 }
